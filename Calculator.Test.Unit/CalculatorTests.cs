3ecb6bc76bb752a1601cafc11eac853d5b53ef01
Mahmoud-SK <mahmoudkarzoun00@gmail.com>
2021-02-03 15:51:26 +0100

    Add project files.

---
+++ Calculator.Test.Unit/CalculatorTests.cs
@@ -0,0 +1,55 @@
+using NUnit.Framework;
+
+namespace Calculator.Unit.Test
+{
+	public class CalculatorTests
+	{
+		private CalculatorClass uut;
+		[SetUp]
+		public void Setup()
+		{
+			uut = new CalculatorClass();
+		}
+
+		[TestCase(2, 4, 6)]
+		[TestCase(-25, 55, 30)]
+		[TestCase(5, -5, 0)]
+		[TestCase(15, -5, 10)]
+		public void Add_AddPositiveAndNegativeNumbers_ReturnCorrectResult(double a, double b, double result)
+		{
+
+			Assert.That(uut.Add(a, b), Is.EqualTo(result));
+
+		}
+
+		[TestCase(-4, 2, -6)]
+		[TestCase(55, -25, 80)]
+		[TestCase(-5, 5, -10)]
+		[TestCase(-15, 5, -20)]
+		public void Substract_SubstractTwoNumbersBothPosAndNeg_ReturnCorrectResult(double a, double b, double result)
+		{
+			Assert.That(uut.Substract(a, b), Is.EqualTo(result));
+		}
+
+		[TestCase(-4, 2, -8)]
+		[TestCase(5, -5, -25)]
+		[TestCase(-75, 25, -10)]
+		[TestCase(-15, 75, -1125)]
+		public void Multiply_MultiplyTwoNumbersBothPosAndNeg_ReturnsCorrect(double a, double b, double result)
+		{
+			Assert.That(uut.Multiply(a, b), Is.EqualTo(result));
+		}
+
+		[TestCase(-4, 2, 16)]
+		[TestCase(3, 1, 3)]
+		[TestCase(-5, 5, -3125)]
+		[TestCase(-15, 5, -759375)]
+		public void Power_PowerTwoNumbersBothPosAndNeg_ReturnsCorrect(double a, double b, double result)
+		{
+			Assert.That(uut.Power(a, b), Is.EqualTo(result));
+		}
+
+
+
+	}
+}
---
+++ CalculatorCore/Calculator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Calculator
+{
+	public class CalculatorClass
+	{
+		static void Main(string[] args)
+		{
+			CalculatorClass calc = new CalculatorClass();
+
+			Console.WriteLine("Testing Addition - calc.Add(x,x)");
+			Console.WriteLine($"8 + 2 = {calc.Add(8, 2)}");
+			Console.WriteLine($"909 + 525 = {calc.Add(909, 525)}");
+			Console.WriteLine($"352 + 86 = {calc.Add(352, 86)}");
+			Console.WriteLine($"155 + 35 = {calc.Add(155, 35)}\n");
+
+
+			Console.WriteLine("Testing Substract - calc.Substract(x,x)");
+			Console.WriteLine($"8 - 2 = {calc.Substract(8, 2)}");
+			Console.WriteLine($"55 - 5 = {calc.Substract(55, 5)}");
+			Console.WriteLine($"25 - 86 = {calc.Substract(25, 86)}");
+			Console.WriteLine($"155 - 5 = {calc.Substract(155, 5)}\n");
+
+
+			Console.WriteLine("Testing Multiply - calc.Multiply(x,x)");
+			Console.WriteLine($"8 * 2 = {calc.Multiply(8, 2)}");
+			Console.WriteLine($"9 * 5 = {calc.Multiply(9, 5)}");
+			Console.WriteLine($"2 * 86 = {calc.Multiply(2, 86)}");
+			Console.WriteLine($"54 * 5 = {calc.Multiply(54, 5)}\n");
+
+
+			Console.WriteLine("Testing Power - calc.Power(x,x)");
+			Console.WriteLine($"5 power 2 = {calc.Power(5, 2)}");
+			Console.WriteLine($"25 power 4 = {calc.Power(25, 4)}");
+			Console.WriteLine($"25 power 6 = {calc.Power(25, 6)}");
+			Console.WriteLine($"14 power 2 = {calc.Power(14, 2)}\n");
+		}
+
+		public double Add(double a, double b)
+		{
+			return a + b;
+		}
+
+		public double Substract(double a, double b)
+		{
+			return a - b;
+		}
+
+		public double Multiply(double a, double b)
+		{
+			return a * b;
+		}
+
+		public double Power(double x, double exp)
+		{
+			return Math.Pow(x, exp);
+		}
+	}
+}
